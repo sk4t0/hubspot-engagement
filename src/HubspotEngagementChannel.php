41a65f0dd7861575f9f66a38a27b89ba48648fb3
Alberto Peripolli <info@albertoperipolli.com>
2021-02-20 11:34:59 +0100

    fix sync

--- src/HubspotEngagementChannel.php
+++ src/HubspotEngagementChannel.php
@@ -5,6 +5,7 @@ namespace NotificationChannels\HubspotEngagement;
 use NotificationChannels\HubspotEngagement\Exceptions\CouldNotSendNotification;
 use Illuminate\Notifications\Notification;
 use SevenShores\Hubspot\Factory as Hubspot;
+use SevenShores\Hubspot\Exceptions\BadRequest;
 
 class HubspotEngagementChannel
 {
@@ -30,45 +31,52 @@ class HubspotEngagementChannel
     {
         $message = $notification->toMail($notifiable);
         $emailMetaData = [
-            "from"=> [
-                "email" => $message->from[0],
-                "firstName" => $message->from[1]
+            "from" => [
+                "email" => $message->from ? $message->from[0] : config('mail.from.address'),
+                "firstName" => $message->from ? $message->from[1] : config('mail.from.name')
             ],
             "to" => [[
                 "email" => $notifiable->routeNotificationForMail($notification)
             ]],
             "cc" => [],
             "bcc" => [],
-            "subject" =>  $message->subject,
-            "html" =>  $message->render()
+            "subject" => $message->subject,
+            "html" => "prova"//$message->render()
         ];
-        if(!empty($message->cc)){
-            foreach($message->cc as $cc){
+        if (!empty($message->cc)) {
+            foreach ($message->cc as $cc) {
                 $emailMetaData["cc"][] = ["email" => $cc[0]];
             }
         }
-        if(!empty($message->bcc)){
-            foreach($message->bcc as $bcc){
+        if (!empty($message->bcc)) {
+            foreach ($message->bcc as $bcc) {
                 $emailMetaData["bcc"][] = ["email" => $bcc[0]];
             }
         }
-        $this->hubspot->engagements()->create(
-            [
-                "active" => true,
-                "ownerId" => $notifiable->hubspot_owner_id ?? null,
-                "type" => "EMAIL",
-                "timestamp" => now()->timestamp
-            ],
-            [
-                "contactIds"=> [$notifiable->hubspot_contact_id ?? null],
-            ],
-            [],
-            $emailMetaData
-        );
-        //$response = [a call to the api of your notification send]
 
-//        if ($response->error) { // replace this by the code need to check for errors
-//            throw CouldNotSendNotification::serviceRespondedWithAnError($response);
-//        }
+        try {
+            $e = $this->hubspot->engagements()->create(
+                [
+                    "active" => true,
+                    "ownerId" => $notifiable->hubspot_owner_id ?? null,
+                    "type" => "EMAIL",
+                    "timestamp" => now()->getPreciseTimestamp(3)
+                ],
+                [
+                    "contactIds" => $notifiable->hubspot_contact_id ? [$notifiable->hubspot_contact_id] : [],
+                    "companyIds" => [],
+                    "dealIds" => [],
+                    "ownerIds" => [],
+                    "ticketIds" => []
+                ],
+                $emailMetaData,
+                []
+            );
+        } catch (BadRequest $e) {
+            throw CouldNotSendNotification::serviceRespondedWithAnError($e->getMessage());
+            return null;
+        }
+        dd($e);
+        return $e;
     }
 }
